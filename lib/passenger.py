730b6c124c673d0978298e846598bb57b9fa91f0
unknown <Morriss7@github.com>
2015-09-20 18:09:34 +0100

    Finished passenger.py

---
+++ lib/passenger.py
@@ -0,0 +1,12 @@
+class Passenger():
+	def __init__(self, name, age, gender, image):
+		self.name = name
+		self.age = age
+		self.gender = gender
+		self.image = image
+		self.afflictions = []
+		self.health = 100
+		self.foodDivisions = 2
+		self.status = "healthy"
+	def __str__(self):
+		return(self.name)
