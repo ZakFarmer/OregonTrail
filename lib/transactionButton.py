dfad00de1d6d1ae9697404567044802ca91d3a5b
unknown <davidmorrissamuels7777@outlook.com>
2015-09-20 19:06:07 +0100

    Finished transactionButton.py

---
+++ lib/transactionButton.py
@@ -0,0 +1,9 @@
+from pygame import transform, image, Rect
+
+class transactionButtion():
+	def __init__(self, transaction, item, imagePosition, rectPosition, resourcePath)
+	self.transaction
+	self.item
+	self.imagePosition
+	self.rectPosition
+	self.resourcePath
